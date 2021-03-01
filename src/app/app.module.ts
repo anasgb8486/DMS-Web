f1ef593c50e2afcd6f31168e6387e566b2916f73
Mohammad Anas <md.anas87@hotmail.com>
2021-03-02 00:14:21 +0530

    added some folder in structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavigationbarComponent } from './shared/components/navigationbar/navigationbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    NavigationbarComponent
   ],
   imports: [
     BrowserModule,
