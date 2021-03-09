93379ccc7eb9dbab6a0eebe54697f153990e9c71
Mohsin Khan <mohsin.khan@ssloman.com>
2021-03-09 23:58:04 +0400

    validator fixes

--- src/app/shared/custom.validators.ts
+++ src/app/shared/custom.validators.ts
@@ -4,7 +4,7 @@ export class CustomValidators {
     static startingWithEmptySpace() {
         return (control: AbstractControl): { [key: string]: any } | null => {
             const ctrlValue: string = control.value;
-            if (ctrlValue.startsWith(' ')) {
+            if (ctrlValue && ctrlValue.startsWith(' ')) {
                 return { 'startingWithEmptySpace': true };
             } else {
                 return null;
