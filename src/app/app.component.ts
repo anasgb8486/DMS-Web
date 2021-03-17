0bc76848a22e9a8de3fa5c448452adb1aabd2a9b
Mohsin Khan <mohsin.khan@ssloman.com>
2021-03-17 23:08:00 +0400

    bottom post your requirement link added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './shared/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DMS-Web';
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(componentName): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      disableClose: true,
+      width: '750px',
+      data: componentName,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
