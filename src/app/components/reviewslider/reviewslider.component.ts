90306111562c6221ecf1cdfc7cd5ae5e3afd2cec
Mohammad Anas <md.anas87@hotmail.com>
2021-03-27 11:24:27 +0530

    review slider integrated

---
+++ src/app/components/reviewslider/reviewslider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reviewslider',
+  templateUrl: './reviewslider.component.html',
+  styleUrls: ['./reviewslider.component.css']
+})
+export class ReviewsliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
