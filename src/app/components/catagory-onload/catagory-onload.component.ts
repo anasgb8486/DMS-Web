d6f8b99f1c6aae5d31b314e8d8b5a7c814df5012
anasgb8486 <md.anas87@gmail.com>
2021-08-07 15:37:53 +0530

    Added arrows in banner

--- src/app/components/catagory-onload/catagory-onload.component.ts
+++ src/app/components/catagory-onload/catagory-onload.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ToggleAllCatagoriesService } from 'src/app/services/toggle-all-catagories.service';
 
 @Component({
   selector: 'app-catagory-onload',
   templateUrl: './catagory-onload.component.html',
-  styleUrls: ['./catagory-onload.component.scss']
+  styleUrls: ['./catagory-onload.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class CatagoryOnloadComponent implements OnInit {
 
@@ -15,11 +17,16 @@ export class CatagoryOnloadComponent implements OnInit {
 
   constructor(
     private _router: Router,
-    private toggleAllCatagoriesService: ToggleAllCatagoriesService) {
+    private toggleAllCatagoriesService: ToggleAllCatagoriesService,
+    config: NgbCarouselConfig) {
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = true;
+    config.pauseOnHover = true;
   }
 
   ngOnInit(): void {
-    this.toggleAllCatagoriesService.currentApprovalStageMessage.subscribe(msg => this.displayAll = msg );
+    this.toggleAllCatagoriesService.currentApprovalStageMessage.subscribe(msg => this.displayAll = msg);
   }
 
   openAllRealtedBrands(catagoryId: number): void {
@@ -28,7 +35,7 @@ export class CatagoryOnloadComponent implements OnInit {
     }
   }
   toggleDisplayAll(): void {
-    this.displayAll = !this.displayAll;    
+    this.displayAll = !this.displayAll;
     this.toggleAllCatagoriesService.updateDisplayAllCatagoriesFlag(this.displayAll);
   }
 
